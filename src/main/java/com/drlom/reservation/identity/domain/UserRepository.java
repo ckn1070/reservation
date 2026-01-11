6f41d6026212a22cf0fe645b07f916cab35a8cce
CKNam <ckn1070@naver.com>
2026-01-12 00:34:49 +0900

    add: UserRepository 추가

---
+++ src/main/java/com/drlom/reservation/identity/domain/UserRepository.java
@@ -0,0 +1,52 @@
+package com.drlom.reservation.identity.domain;
+
+import java.util.Optional;
+
+/**
+ * User Aggregate Root Repository Interface
+ *
+ * <p>- Repository: - Domain 계층에 인터페이스 정의 (DIP) - Infrastructure 계층에서 구현
+ *
+ * <p>- Aggregate Root 단위로 저장/조회 - Collection처럼 사용 (save, findById, delete 등)
+ */
+public interface UserRepository {
+
+  /**
+   * User 저장
+   *
+   * @param user 저장할 User
+   * @return 저장된 User (ID 부여됨)
+   */
+  User save(User user);
+
+  /**
+   * ID로 User 조회
+   *
+   * @param id User ID
+   * @return User (존재하지 않으면 Optional.empty())
+   */
+  Optional<User> findById(Long id);
+
+  /**
+   * Email로 User 조회
+   *
+   * @param email Email Value Object
+   * @return User (존재하지 않으면 Optional.empty())
+   */
+  Optional<User> findByEmail(Email email);
+
+  /**
+   * Email 존재 여부 확인
+   *
+   * @param email Email Value Object
+   * @return 존재 여부
+   */
+  boolean existsByEmail(Email email);
+
+  /**
+   * User 삭제
+   *
+   * @param user 삭제할 User
+   */
+  void delete(User user);
+}
