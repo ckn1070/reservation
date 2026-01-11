fdbd9c8ef2ead11dfcb76ca7d732287b390e03f1
CKNam <ckn1070@naver.com>
2026-01-11 17:20:28 +0900

    feat: JVM 기본 시간대를 UTC로 맞추기 위해 App에 post construct 추

--- src/main/java/com/drlom/reservation/ReservationApplication.java
+++ src/main/java/com/drlom/reservation/ReservationApplication.java
@@ -1,5 +1,7 @@
 package com.drlom.reservation;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,11 @@ public class ReservationApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ReservationApplication.class, args);
+  }
+
+  // JVM 기본 시간대를 UTC로 설정 (DB TIMESTAMP와 일관성 유지)
+  @PostConstruct
+  void setDefaultTimezone() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 }
