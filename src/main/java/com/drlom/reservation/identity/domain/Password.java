e74e361b544762d69f37dc84373e6f8325414dd1
CKNam <ckn1070@naver.com>
2026-01-31 20:19:38 +0900

    add: 비밀번호 vo에 임시 비밀번호 생성 추가

--- src/main/java/com/drlom/reservation/identity/domain/Password.java
+++ src/main/java/com/drlom/reservation/identity/domain/Password.java
@@ -2,6 +2,7 @@ package com.drlom.reservation.identity.domain;
 
 import com.drlom.reservation.common.error.BusinessException;
 import com.drlom.reservation.common.error.ErrorCode;
+import java.security.SecureRandom;
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,6 +23,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Password {
 
   private static final BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+  private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+  private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+  private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private static final String DIGITS = "0123456789";
+  private static final String SPECIAL = "!@#$%^&*";
+  private static final String ALL_CHARACTERS = LOWERCASE + UPPERCASE + DIGITS + SPECIAL;
+  private static final int TEMPORARY_PASSWORD_LENGTH = 20;
 
   private final String hash;
 
@@ -62,6 +71,37 @@ public class Password {
     if (rawPassword == null || rawPassword.isBlank()) {
       throw new BusinessException(ErrorCode.INVALID_PASSWORD, "비밀번호는 필수입니다");
     }
+  }
+
+  /**
+   * 임시 비밀번호 생성 (20글자, 영문 대소문자 + 숫자 + 특수문자)
+   *
+   * @return 임시 비밀번호 평문
+   */
+  public static String generateTemporaryPassword() {
+    StringBuilder password = new StringBuilder(TEMPORARY_PASSWORD_LENGTH);
+
+    // 각 문자 종류 최소 1개 보장
+    password.append(LOWERCASE.charAt(SECURE_RANDOM.nextInt(LOWERCASE.length())));
+    password.append(UPPERCASE.charAt(SECURE_RANDOM.nextInt(UPPERCASE.length())));
+    password.append(DIGITS.charAt(SECURE_RANDOM.nextInt(DIGITS.length())));
+    password.append(SPECIAL.charAt(SECURE_RANDOM.nextInt(SPECIAL.length())));
+
+    // 나머지 글자 랜덤 채우기
+    for (int i = 4; i < TEMPORARY_PASSWORD_LENGTH; i++) {
+      password.append(ALL_CHARACTERS.charAt(SECURE_RANDOM.nextInt(ALL_CHARACTERS.length())));
+    }
+
+    // 셔플 (Fisher-Yates)
+    char[] chars = password.toString().toCharArray();
+    for (int i = chars.length - 1; i > 0; i--) {
+      int j = SECURE_RANDOM.nextInt(i + 1);
+      char temp = chars[i];
+      chars[i] = chars[j];
+      chars[j] = temp;
+    }
+
+    return new String(chars);
   }
 
   // 값 기반 동등성 비교
